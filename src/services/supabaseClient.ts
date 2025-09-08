e6fcc0feca2d11538be8e25ee2f103107eb42259
Jagveer Kaur Khalsa <144109408+jagv091205@users.noreply.github.com>
2025-09-08 18:26:42 +0530

    Update supabaseClient.ts

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,6 +1,6 @@
 // src/services/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://brzerxzuydonfxcdgjqb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyemVyeHp1eWRvbmZ4Y2RnanFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MDUwODYsImV4cCI6MjA2NzQ4MTA4Nn0.yvxtQxm7ySwLovvLiXhbI1tyiHFKptnrWFtAP4dWnwY';
+const supabaseUrl = 'YOUR SUPABASE URL';
+const supabaseKey = 'YOUR SUPABASEKEY';
 export const supabase = createClient(supabaseUrl, supabaseKey);
